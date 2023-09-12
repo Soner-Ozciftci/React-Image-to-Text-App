ac88b1589ac045451dfd65e58b3db4ba0c40e440
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-12 23:12:47 +0200

    onChange

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [textResult, setTextResult] = useState("");
 
+const handleChange = e => {
+  setSelectedImage(e.target.files[0])
+}
+
   return (
     <div className="App">
       <h1>Image to Text App</h1>
       <p>Get words in image !</p>
 <div className="input-wrapper">
   <label htmlFor="upload">Upload Image</label>
-  <input type="file" id="upload" accept="image/*"/>
+  <input type="file" id="upload" accept="image/*" onChange={handleChange}/>
 </div>
 
 
 <div className="result">
 {selectedImage && (
-<img src={selectedImage} />
+<img src={URL.createObjectURL(selectedImage)} alt="thumbnail"/>
 
 )}
 </div>
