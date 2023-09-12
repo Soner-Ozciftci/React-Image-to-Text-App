63571609bd7da6d722069d43d5c3d7dccc1325e8
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-12 23:07:57 +0200

    selectedImage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React , {useState,useEffect} from 'react'
- import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [textResult, setTextResult] = useState("");
+
   return (
     <div className="App">
+      <h1>Image to Text App</h1>
+      <p>Get words in image !</p>
+<div className="input-wrapper">
+  <label htmlFor="upload">Upload Image</label>
+  <input type="file" id="upload" accept="image/*"/>
+</div>
 
+
+<div className="result">
+{selectedImage && (
+<img src={selectedImage} />
+
+)}
+</div>
     </div>
   );
 }
